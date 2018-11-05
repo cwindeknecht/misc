743e940524eae3998a5daeedf65f2b21560b547e
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-11-05 17:32:57 -0500

    User Authentication
    Tests all work, Manually tested with Postman

--- apione/app/controllers/application_controller.rb
+++ apione/app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 # app/controllers/application_controller.rb
+
+# On every request, the application will verify the request by calling the request authorization service. 
+# If the request is authorized, it will set the current user object to be used in the other controllers.
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  # called before every action on controllers
+  before_action :authorize_request
+  attr_reader :current_user
+
+  private
+
+  # Check for valid request token and return user
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
---
+++ apione/spec/controllers/application_controller_spec.rb
@@ -0,0 +1,37 @@
+# spec/controllers/application_controller_spec.rb
+
+# Prior to this, our API does not authorize requests with a token.
+# We have to make sure that on every request (except authentication) our API checks for a valid token. 
+# To achieve this, we'll implement a callback in the application controller that authenticates every request. 
+# Since all controllers inherit from application controller, it will be propagated to all controllers.
+require "rails_helper"
+
+RSpec.describe ApplicationController, type: :controller do
+  # create test user
+  let!(:user) { create(:user) }
+   # set headers for authorization
+  let(:headers) { { 'Authorization' => token_generator(user.id) } }
+  let(:invalid_headers) { { 'Authorization' => nil } }
+
+  describe "#authorize_request" do
+    context "when auth token is passed" do
+      before { allow(request).to receive(:headers).and_return(headers) }
+
+      # private method authorize_request returns current user
+      it "sets the current user" do
+        expect(subject.instance_eval { authorize_request }).to eq(user)
+      end
+    end
+
+    context "when auth token is not passed" do
+      before do
+        allow(request).to receive(:headers).and_return(invalid_headers)
+      end
+
+      it "raises MissingToken error" do
+        expect { subject.instance_eval { authorize_request } }.
+          to raise_error(ExceptionHandler::MissingToken, /Missing token/)
+      end
+    end
+  end
+end
