c50b84ccc13424cb9fad9569398003dcebf77715
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-11-06 13:30:46 -0500

    Added Pagination

--- apione/db/seeds.rb
+++ apione/db/seeds.rb
@@ -6,5 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Todo.create([title:'Yard Work', created_by:'Cody'])
-Item.create([{name:'Mow Lawn', todo: 'Yard Work'},{name:'Rake Leaves', todo:'Yard Work'}])
+# Todo.create([title:'Yard Work', created_by:'Cody'])
+# Item.create([{name:'Mow Lawn', todo: 'Yard Work'},{name:'Rake Leaves', todo:'Yard Work'}])
+
+50.times do
+  todo = Todo.create(title: Faker::Lorem.word, created_by: 5)
+  todo.items.create(name: Faker::Lorem.word, done: false)
+end
