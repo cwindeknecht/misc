adeaaab9acb7760529e53e098ec723c63e58e940
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-11-10 12:46:55 -0500

    Added Basic Routing

--- heroes/src/app/hero-details/hero-details.component.ts
+++ heroes/src/app/hero-details/hero-details.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
- 
+
 @Component({
   selector: 'app-hero-details',
   templateUrl: './hero-details.component.html',
-  styleUrls: ['./hero-details.component.less']
+  styleUrls: ['./hero-details.component.less'],
 })
 export class HeroDetailsComponent implements OnInit {
   @Input() hero: Hero;
- 
-  constructor() { }
- 
-  ngOnInit() {
-  }
- 
+
+  constructor() {}
+
+  ngOnInit() {}
 }
